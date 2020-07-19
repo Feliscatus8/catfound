22f96f212776dc2f8deb858708f0d9bd6165348d
Feliscatus8 <feliscatus@interia.eu>
2020-07-19 09:52:22 +0200

    add logout and user recognition on volunteer interface

--- meow/views.py
+++ meow/views.py
@@ -3,7 +3,3 @@ from django.shortcuts import render
 
 def interface(request):
     return render(request, 'meow/meow.html', {})
-
-
-def login(request):
-    return render(request, 'registration/login.html', {})
