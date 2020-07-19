777c7eac217260099f50dac2b69f5523999cc766
Feliscatus8 <feliscatus@interia.eu>
2020-07-19 11:06:12 +0200

    add volunteer profile page

--- meow/views.py
+++ meow/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def interface(request):
     return render(request, 'meow/meow.html', {})
+
+
+def profile(request):
+    return render(request, 'meow/profile.html', {})
